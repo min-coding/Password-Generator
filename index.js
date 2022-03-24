be4755925fb676fae09ebcb11448a4b8810bcfc5
Min <nichapha.t@reverseads.com>
2022-03-24 15:29:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// https://www.ascii-code.com/
+
+
+const generateBtn = document.querySelector('#generate-password')
+const passwordDisplay = document.querySelector('.password-results')
+let passwordArray = []
+
+function generatePassword(){
+  for (a=0;a<4;a++){
+    const charArray = []
+      for (i=0;i<15;i++){
+        const num = Math.floor(Math.random() * (122 - 35) + 35)
+        if(num !== 58,59,60,61,62){
+          const char = String.fromCharCode(num)
+          charArray.push(char)
+        }
+      }
+    const password = charArray.join("")
+    passwordArray.push(password)
+    console.log(password.length)
+  }
+  console.log(passwordArray)
+}
+
+generateBtn.addEventListener('click',function(){
+  generatePassword()
+  passwordDisplay.innerHTML = passwordArray.map(item =>{
+    return `<div class='password'> ${item} </div>`
+  }).join('')
+  passwordArray = []
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Requirement
+ * 
+ * - 15 random characters 
+ * - push 15 loop of random letters 33 to 126 
+ * 
+ * function getRandomArbitrary(33, 126) {
+  return Math.random() * (126 - 33) + 33;
+ * 
+
+  const charArray = []
+  const passwordArray =[]
+  function generatePassword(){
+    for (i<1;i<16;i++){
+      charArray.push(Math.random() * (126 - 33) + 33)
+    }
+    for (i<0;i<15;i++){
+      passwordArray[i] = String.fromCharCode(passwordArray[i])
+    }
+  }
+ * 
+ */
